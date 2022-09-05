87a63d353dc5ddf10a2d42d60ea769c2712afeff
thePeras <jtopereira@gmail.com>
2022-09-05 19:39:58 +0100

    footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,8 @@ module.exports = {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
-          include: /\.inline\.svg$/
+          include: /\.inline\.svg$/,
+          include: /\.svg$/
         }
       }
     }
--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -1,14 +1,61 @@
 import React from "react";
+//SVG's
+import SinfLogo from "../../images/svg/logo.svg";
+import InstaIcon from "../../images/svg/instagram.svg";
+import LinkedinIcon from "../../images/svg/linkedin.svg";
+import FacebookIcon from "../../images/svg/facebook.svg";
+import MailIcon from "../../images/svg/mail.svg";
+import LocationIcon from "../../images/svg/location.svg";
 
 import FooterStyles from "../../styles/common/footer.module.css";
 
+const links = [
+  {
+    // INSTAGRAM
+    icon: <InstaIcon />,
+    href: "https://instagram.com/sinffeup"
+  },
+  {
+    // LINKEDIN
+    icon: <LinkedinIcon />,
+    href: "https://www.linkedin.com/company/sinffeup/"
+  },
+  {
+    // FACEBOOK
+    icon: <FacebookIcon />,
+    href: "https://facebook.com/sinfFEUP"
+  },
+  {
+    // EMAIL
+    icon: <MailIcon />,
+    href: "https://www.linkedin.com/company/sinffeup/"
+  }
+];
+
 const Footer = ({ darkMode }) => (
-  <div
-    className={`${FooterStyles.footerContainer} ${
-      darkMode ? FooterStyles.dark : FooterStyles.light
-    } py-2 py-sm-3 text-center`}
-  >
-    Copyright © 2021 niaefeup, todos os direitos reservados
+  <div class={FooterStyles.footerContainer}>
+    <div class={FooterStyles.topLayer}>
+      <div class={FooterStyles.social}>
+        <div class={FooterStyles.icon_links}>
+          {links.map(({ icon, href }, index) => (
+            <a href={href} className={FooterStyles.icon_link} key={index}>
+              {icon}
+            </a>
+          ))}
+        </div>
+        <div className={FooterStyles.location}>
+          <LocationIcon className={FooterStyles.location_icon} />
+          <div>
+            <p>FEUP</p>
+            <p>R. Dr. Roberto Frias 4200-465 Porto, Portugal</p>
+          </div>
+        </div>
+      </div>
+      <SinfLogo className={FooterStyles.logo} />
+    </div>
+    <div className={`py-2 py-sm-3 text-center`}>
+      Copyright © 2022 niaefeup, todos os direitos reservados
+    </div>
   </div>
 );
 
